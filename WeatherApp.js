3d7ef17f4a1665940de5d58770a6f1bbb8537a68
Charlie <charliecuddy4@gmail.com>
2018-11-26 22:37:36 -0600

    add degree symbol and F to degree

--- WeatherApp.js
+++ WeatherApp.js
@@ -2,20 +2,21 @@
 $(document).ready(function(){
     $("#zip-form").submit(function(event){
         event.preventDefault();
-        
+
         var zipName = $("#zip-name").val();
         getWeather(zipName);
     });
-    
+
   function getWeather(zip) {
-    
+
         $.get("http://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&units=imperial&appid=7cc4953c245c90023cf3b1cea6b0eb42", function(data){
-                
-            let tempRound = Math.round(data.main.temp); 
+
+            let tempRound = Math.round(data.main.temp) + "&#8457;";
             
+
             $("#city").html(data.name);
             $("#temp").html(tempRound);
         });
-        
+
     };
 });
