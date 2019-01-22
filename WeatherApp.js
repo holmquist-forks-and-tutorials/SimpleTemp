9c7f6c0e952fb6472853f8583e388e74eacd3af2
Collin Holmquist <collinholmquist@Collins-MacBook-Air.local>
2019-01-22 12:03:53 -0600

    Added linear gradient backgound based upon temperature

--- WeatherApp.js
+++ WeatherApp.js
@@ -21,8 +21,56 @@ $(document).ready(function(){
             $("#temp").html(tempRound);
             $("#tempConvert").html(convertDisplay);
 
-            //Append the original background of the container to a temp-related gradient.
-            $(".weather-container").css({"background-image":"linear-gradient(to right,#FF00E0, yellow)"});
+        //Use if/else if to determine gradient colors given the temperatures.
+        //Then append the linear-gradient based on the temp.
+
+
+            if (theTemp <= -20) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#0500ff, #0100ff )"});
+            }
+            else if (theTemp <=-10) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#0100ff, #0032ff)"});
+            }
+            else if (theTemp <= 0) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #0032ff ,#0084ff )"});
+            }
+            else if (theTemp <= 10) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#0084ff,#00d4ff)"});
+            }
+            else if (theTemp <= 20) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#00d4ff,#00ff83)"});
+            }
+            else if (theTemp <= 30) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #00ff83,#3eff00)"});
+            }
+            else if (theTemp <= 40) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #3eff00 ,#fdff00)"});
+            }
+            else if (theTemp <= 50) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#fdff00 ,#FFd200 )"});
+            }
+            else if (theTemp <= 60) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#FFd200, #FFa000)"});
+            }
+            else if (theTemp <= 70) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #FFa000 , #FF6e00)"});
+            }
+            else if (theTemp <= 80) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#FF6e00 ,#FF3c00 )"});
+            }
+            else if (theTemp <= 90) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #FF3c00 , #FF0a00)"});
+            }
+            else if (theTemp <= 100) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#FF0a00, #FF0040)"});
+            }
+            else if (theTemp <= 110) {
+                $(".weather-container").css({"background-image":"linear-gradient(to right,#FF0040, #FF00E0)"});
+            }
+            else {
+                $(".weather-container").css({"background-image":"linear-gradient(to right, #FF00E0 , #FF04F0)"});
+            }
+
 });
 
   };
