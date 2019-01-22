3be9feb3e9d3b6122092b73923d35c8e5f9c82d1
Collin Holmquist <collinholmquist@Collins-MacBook-Air.local>
2019-01-21 18:06:26 -0600

    Added a JQuery element that inserts a background-color / image when city temp loads.

--- WeatherApp.js
+++ WeatherApp.js
@@ -11,15 +11,20 @@ $(document).ready(function(){
 
         $.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=7cc4953c245c90023cf3b1cea6b0eb42", function(data) {
 
-            let tempRound = Math.round(data.main.temp) + "&#176;" + " F";
+            let tempRound = Math.round(data.main.temp) + "&#176;" + "F";
             let tempConvert = parseFloat(tempRound);
-            let convertDisplay = ((tempConvert-32) / 1.8).toFixed(0) + "&#176;" + " C";
+            let convertDisplay = ((tempConvert-32) / 1.8).toFixed(0) + "&#176;" + "C";
+            let theTemp = Math.round(data.main.temp);
 
 
             $("#city").html(data.name);
             $("#temp").html(tempRound);
-            $("#tempConvert").html(convertDisplay)
-        });
+            $("#tempConvert").html(convertDisplay);
 
-    };
+            //Append the original background of the container to a temp-related gradient.
+            $(".weather-container").css({"background-image":"linear-gradient(to right,#FF00E0, yellow)"});
+});
+
+  };
+
 });
